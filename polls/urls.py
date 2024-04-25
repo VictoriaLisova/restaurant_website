9c8bcfbe0d78ddc86766311dcae0924f58287021
ValeriiaPataman <valeriia.pataman@knu.ua>
2024-04-25 13:39:29 +0300

    added template

--- polls/urls.py
+++ polls/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('', include('polls.urls')),
     path("admin/", admin.site.urls),
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about')
 ]
