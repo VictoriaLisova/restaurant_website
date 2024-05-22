394c65c13055bc94d8c0dbacd50cab71fef071c9
Victoria Lisova <lisovaviktoria268@gmail.com>
2024-05-22 20:53:22 +0300

    update settings

--- restaurant_website/settings.py
+++ restaurant_website/settings.py
@@ -32,6 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 ALLOWED_HOST = os.environ.get('ALLOWED_HOST', '*')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+
 ALLOWED_HOSTS = [ALLOWED_HOST]
 
 
